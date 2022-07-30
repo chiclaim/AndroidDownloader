e52b3d8b223e943030bb9154a0d82b539a1eb71a
chiclaim <chiclaim@gmail.com>
2022-07-30 22:26:36 +0800

    [add] sample 中添加 Leakcanary 和 StrictMode

---
+++ app/src/main/java/com/chiclaim/android/updater/app/MyApplication.kt
@@ -0,0 +1,30 @@
+package com.chiclaim.android.updater.app
+
+import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.VmPolicy
+
+
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork() // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    //.detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build()
+            )
+        }
+    }
+}
